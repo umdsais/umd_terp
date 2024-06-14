04ce12ab72ad0d0c8afa03aa7019a85639142159
Brad Walker <brad.walker@idfive.com>
2024-06-14 14:23:49 -0400

    Accessibility Update - Card Link Text Size (https://trello.com/c/u5UCDi1H/2-card-link-text-size)

--- static/build/js/index.js
+++ static/build/js/index.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "455a20848643daa33cf1";
+/******/ 	var hotCurrentHash = "ff569cc2d82a176c5828";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
